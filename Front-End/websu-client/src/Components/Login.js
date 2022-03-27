c5d9c24fdee6903be1162ac38ee3baa3e04d6df5
Nurullo <thenurullo@gmail.com>
2022-03-27 14:58:13 -0600

    Update Login.js

--- Front-End/websu-client/src/Components/Login.js
+++ Front-End/websu-client/src/Components/Login.js
@@ -1,9 +1,18 @@
 import React from "react";
 
-function Login(){
-    return(
-        <div/>
-    );
+function Login() {
+  return (
+    <div className="form_container">
+      {" "}
+      Form
+      <form>
+        <label for="user_name">User Name:</label>
+        <input type="text" id="user_name" name="user_name"></input>
+        <label for="password">Password:</label>
+        <input type="text" id="password" name="password"></input>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
