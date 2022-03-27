bf0826289f055a312accc7340e6d39216564d179
Nurullo <thenurullo@gmail.com>
2022-03-27 15:33:19 -0600

    basic login

--- Front-End/websu-client/src/Components/Login.js
+++ Front-End/websu-client/src/Components/Login.js
@@ -1,11 +1,17 @@
 import React from "react";
 
-function Login(){
-    return(
-        <div>
-            <h1>hello World</h1>
-        </div>
-    );
+function Login() {
+  return (
+    <div class="login_container">
+      Form
+      <form>
+        <label for="user_name">User Name:</label>
+        <input type="text" id="user_name" name="user_name"></input>
+        <label for="password">Password:</label>
+        <input type="text" id="password" name="password"></input>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
