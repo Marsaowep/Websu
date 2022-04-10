04d74600a2ada85facdfd0f3d289a3d68bfa97f9
Nurullo <thenurullo@gmail.com>
2022-04-10 16:17:52 -0600

    Setup basic Register functionality

--- Front-End/websu-client/src/components/Login.js
+++ Front-End/websu-client/src/components/Login.js
@@ -1,7 +1,17 @@
 import React, { Component, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 async function loginUser(credentials) {
+  return fetch("http://localhost:3000/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  }).then((data) => data.json());
+}
+
+async function registerUser(credentials) {
   return fetch("http://localhost:3000/login", {
     method: "POST",
     headers: {
@@ -14,24 +24,52 @@ async function loginUser(credentials) {
 function Login() {
   const [username, setUserName] = useState();
   const [password, setPassword] = useState();
+  let navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
+  const handleSubmitRegister = async (e) => {
     console.log(username);
     console.log(password);
     e.preventDefault();
+    console.log("E IS THIS: ", e);
     const response = await loginUser({
       username,
       password,
     });
 
-    console.log("response:");
-    console.log(response);
+    console.log("response:", response);
+    console.log(response.response);
+    if (response.response) {
+      navigate("/game", {});
+    } else {
+      window.alert("User Already Exists!");
+    }
+  };
+
+  const handleSubmitLogin = async (e) => {
+    console.log(username);
+    console.log(password);
+    e.preventDefault();
+    window.alert("Logging in!");
+
+    // console.log("E IS THIS: ", e);
+    // const response = await loginUser({
+    //   username,
+    //   password,
+    // });
+
+    // console.log("response:", response);
+    // console.log(response.response);
+    // if (response.response) {
+    //   navigate("/game", {});
+    // } else {
+    //   window.alert("User Already Exists!");
+    // }
   };
 
   return (
     <div className="login_container">
       Form
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmitRegister}>
         <label htmlFor="user_name">User Name:</label>
         <input
           type="text"
@@ -46,7 +84,8 @@ function Login() {
           name="password"
           onChange={(e) => setPassword(e.target.value)}
         ></input>
-        <input type="submit" value="Login"></input>
+        <button onClick={handleSubmitLogin}>Login</button>
+        <input type="submit" value="Register"></input>
       </form>
       <Link to="/Game">Game</Link>
       <br></br>
--- server.js
+++ server.js
@@ -7,18 +7,23 @@ const io = new Server(server);
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
+const { MongoClient } = require("mongodb");
 app.use(cors());
 app.use(bodyParser.json());
 
+require("dotenv").config();
+const uri = process.env.ATLAS_URI; //read in secret mongo url from .env
+
 const PORT = process.env.PORT || 3001;
 
-app.post("/login", function (req, res) {
+app.post("/register", async function (req, res) {
   console.log(req.body);
   console.log(req.body.username);
   console.log(req.body.password);
-
+  var reg = await registerUser(req.body.username, req.body.password);
+  console.log("Reg is...: ", reg);
   res.send({
-    text: "LETS GOOOOO",
+    response: reg,
   });
 });
 
@@ -39,3 +44,117 @@ io.on("connection", (socket) => {
     console.log(msg);
   });
 });
+
+//MONGO DB FUNCTIONS :))
+// Make new user
+
+async function mongoConnect() {
+  const client = new MongoClient(uri);
+  try {
+    // Connect to the MongoDB cluster
+    await client.connect();
+    console.log("CONNECTED CONNEECTED CONNECTED");
+  } catch {
+    console.log("oops it broke :(");
+  }
+
+  return client;
+}
+
+// Return user if passwords match, otherwise return an error
+async function getUser(username, password) {
+  client = await mongoConnect();
+
+  const user = await client
+    .db("WebSuDB")
+    .collection("User_Profile")
+    .findOne({ name: username });
+
+  // Check if user actually exists
+  if (user) {
+    if (user.password == password) {
+      return user;
+    } else {
+      return "Incorrect Password."; // Place holder for now, replace with better logic later
+    }
+  }
+}
+
+async function registerUser(username, userPassword) {
+  client = await mongoConnect();
+  const profileCollection = await client
+    .db("WebSuDB")
+    .collection("User_Profile");
+  const defaultScore = 0;
+
+  if (await checkUserExists(client, username)) {
+    console.log("error, user already exists");
+    return false;
+  }
+
+  // Set up JSON object of user info
+  const newUser = {
+    name: username,
+    password: userPassword,
+    high_score: defaultScore,
+  };
+
+  const result = profileCollection.insertOne(newUser);
+  console.log(`New user created with the following id: ${result.insertedId}`);
+  return true;
+}
+
+// Find existing user
+async function checkUserExists(client, username) {
+  console.log("Checking if current name exists...");
+  const result = await client
+    .db("WebSuDB")
+    .collection("User_Profile")
+    .findOne({ name: username });
+
+  console.log("return is: ", result);
+
+  if (result) {
+    // Check if result queried is null or not.
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Update a users high score
+async function updateUserHighScore(username, newScore) {
+  client = await mongoConnect();
+  const originalUser = await client
+    .db("WebSuDB")
+    .collection("User_Profile")
+    .findOne({ name: username });
+  const updatedUser = originalUser; //safe copy
+  updatedUser.high_score = newScore;
+  const theUser = await client
+    .db("WebSuDB")
+    .collection("User_Profile")
+    .updateOne({ name: username }, { $set: updatedUser });
+}
+
+// Add high score:
+async function addHighScore(score, userName) {
+  client = await mongoConnect();
+  const newHighScore = {
+    name: userName,
+    high_score: score,
+  };
+
+  const high_scores = await client
+    .db("WebSuDB")
+    .collection("high_scores")
+    .insertOne(newHighScore);
+}
+
+// Get 5 High Scores:
+async function get5HighScores() {
+  client = await mongoConnect();
+  const high_scores = await client.db("WebSuDB").collection("high_scores");
+  top5 = high_scores.find({}).sort({ high_score: -1 }).limit(5).toArray();
+  return top5;
+}
