b0b53d14b04203e8f68d93b7593e1ccb0365b502
Nurullo <thenurullo@gmail.com>
2022-04-12 01:49:37 -0600

    JoinLobby and LobbyMenu CSS created

--- Front-End/websu-client/src/components/JoinLobby.js
+++ Front-End/websu-client/src/components/JoinLobby.js
@@ -1,18 +1,28 @@
 import React, { Component, useState } from "react";
 import { Link, Navigate, useNavigate, useLocation } from "react-router-dom";
 
-function JoinLobby(){
-    
-    const location = useLocation();
-    console.log(location.state);
-    const navigate = useNavigate();
-    const startGame = () => navigate("/Game");
-    return(
-        <div>
-            <h1>{location.state.lobbyId}</h1>
-            <button type='button' onClick={startGame}>Start</button>
+function JoinLobby() {
+  const location = useLocation();
+  console.log(location.state.players);
+  const navigate = useNavigate();
+  const startGame = () => navigate("/Game");
+
+  const listItems = location.state.players.map((d) => <li key={d}>{d}</li>);
+  return (
+    <div className="roomContainer">
+      <div className="text-center text-light align-items-center mb-3 d-flex flex-column ">
+        <h2>Lobby Code: {location.state.lobbyId}</h2>
+        <div className="players_container">
+          <h2>Lobby Players: </h2>
+          {listItems}
         </div>
-    );
+
+        <button type="button" onClick={startGame}>
+          Start
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default JoinLobby;
--- Front-End/websu-client/src/components/LobbyMenu.js
+++ Front-End/websu-client/src/components/LobbyMenu.js
@@ -2,79 +2,92 @@ import React, { Component, useState } from "react";
 import { Link, Navigate, useLocation, useNavigate } from "react-router-dom";
 import { socket } from "../App";
 
-export default function LobbyMenu(){
+export default function LobbyMenu() {
+  var location = useLocation();
 
-    var location = useLocation();
+  const [lobbyId, setLobbyId] = useState();
+  var theLobbyId = "";
+  var players = [];
+  var host = "";
+  var playerScores = [];
+  var username = location.state.response.name;
+  var roomExists = false;
 
-    const [lobbyId, setLobbyId] = useState();
-    var theLobbyId = "";
-    var players = [];
-    var host = "";
-    var playerScores = [];
-    var username = location.state.response.name;
-    var roomExists = false;
+  let navigate = useNavigate();
 
-    let navigate = useNavigate();
+  console.log("username: ", username);
+  const createLobby = () => {
+    socket.emit("createLobby", {
+      username: username,
+    });
+  };
 
-    console.log('username: ', username);
-    const createLobby = () => {
-        socket.emit('createLobby', {
-            username: username
-        });
+  const joinALobby = (e) => {
+    e.preventDefault();
+    socket.emit("joinLobby", {
+      username: username,
+      roomId: lobbyId,
+    });
+  };
+
+  socket.on("lobbyId", async (data) => {
+    console.log(data.room);
+    console.log(data.players);
+    console.log(data.host);
+    theLobbyId = data.room;
+    players = data.players;
+    host = data.host;
+    navigate("/JoinLobby", {
+      state: { username: username, players: players, lobbyId: theLobbyId },
+    });
+  });
+
+  socket.on("lobbyName", (data) => {
+    roomExists = data.roomExists;
+    players = data.usernames;
+    host = data.lobbyHost;
+    theLobbyId = data.room;
+
+    if (roomExists) {
+      navigate("/JoinLobby", {
+        state: { username: username, players: players, lobbyId: theLobbyId },
+      });
+    } else {
+      window.alert("Lobby Doesn't exist");
     }
+  });
 
-    const joinALobby = (e) => {
-        e.preventDefault();
-        socket.emit('joinLobby', {
-            username: username,
-            roomId: lobbyId
-        });
-    }
+  socket.on("updateScores", (data) => {
+    playerScores = data.scores;
+  });
 
-    socket.on('lobbyId', async (data)=>{
-        console.log(data.room);
-        console.log(data.players);
-        console.log(data.host);
-        theLobbyId = data.room;
-        players = data.players;
-        host = data.host;
-        navigate("/JoinLobby", {state: {username: username, players: players, lobbyId: theLobbyId} });
-    });
+  return (
+    <div className="lobby_menu_container">
+      <div className="menu_buttons text-center text-light align-items-center mb-3 d-flex flex-column ">
+        <button
+          className="mb-5 mt-5"
+          onClick={() => {
+            createLobby(username);
+          }}
+        >
+          Create A Lobby
+        </button>
+        <form
+          className="text-center text-light align-items-center d-flex flex-column w-25"
+          onSubmit={joinALobby}
+        >
+          <label htmlFor="user_name">Lobby Code:</label>
+          <input
+            className="w-75"
+            type="text"
+            id="room_id"
+            name="room_id"
+            onChange={(e) => setLobbyId(e.target.value)}
+          ></input>
 
-    socket.on('lobbyName', (data) =>{
-        roomExists = data.roomExists;
-        players = data.usernames;
-        host = data.lobbyHost;
-        theLobbyId = data.room;
-
-        if(roomExists){
-            navigate("/JoinLobby", {state: {username: username, players: players, lobbyId: theLobbyId} });
-        }
-        else{
-            window.alert("Lobby Doesn't exist");
-        }
-    });
-
-    socket.on('updateScores', (data) =>{
-        playerScores = data.scores
-    });
-
-    return(
-        <div className="lobby_menu_container">
-            <div className="menu_buttons">
-                <button onClick={() => {createLobby(username)}}>Create A Lobby</button>
-                <form onSubmit={joinALobby}>
-                    <label htmlFor="user_name">Lobby Code:</label>
-                    <input
-                        type="text"
-                        id="room_id"
-                        name="room_id"
-                        onChange={(e) => setLobbyId(e.target.value)}
-                    ></input>
-                    <input type="submit" data="Join"></input>
-                </form>
-            </div>
-        </div>
-    );
+          <input className="mt-4 w-75" type="submit" data="Join"></input>
+        </form>
+      </div>
+    </div>
+  );
 }
-
